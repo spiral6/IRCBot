3c83f21bb3ecb6f80b39ef77094766abd9f38f22
Michael Peroutek <mbpyro@yahoo.com>
2015-12-03 11:10:01 -0600

    Started working on CurrencyThread

--- src/CurrencyThread.java
+++ src/CurrencyThread.java
@@ -1,21 +1,28 @@
 import org.jibble.pircbot.*;
 import java.util.ArrayList;
+import java.sql.*;
+import javax.sql.rowset.serial.SQLInputImpl;
 public class CurrencyThread extends Thread{
 	static ConnectIRC botbot;
 	ArrayList<String> currentusers;
-	
+	static DriverManager conndriver;
+	static SQLInputImpl connectiondb;
 	public static void main(String[] args) {
 
 	}
 	public void runDefault(ConnectIRC bot){
 		botbot=bot;
 		this.start();
+		conndriver = null;
+		conndriver.getConnection(sdf,adsf,sdaf);
+		connectiondb.readArray();
+		
 	}
 	public void run(){
 		while(true){
-			for(User a : botbot.getUsers(ConnectIRC.channel))
+			for(User a : botbot.getUsers(ConnectIRC.channel)){
 			currentusers.add(a.getNick());
+			}
 		}
-		
 	}
 }
